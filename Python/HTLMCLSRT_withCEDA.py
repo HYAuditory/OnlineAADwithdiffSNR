7e0477823469416e625d02055f9bf6b37927b430
CEDA <85104167+CostEffectiveDeviceforAAD@users.noreply.github.com>
2022-05-09 18:36:01 +0900

    Add files via upload

--- Python/HTLMCLSRT_withCEDA.py
+++ Python/HTLMCLSRT_withCEDA.py
@@ -12,6 +12,7 @@ import time, serial, logging, scipy.io
 from sklearn.metrics import mean_squared_error
 from math import sqrt
 import serial, string, math
+import pandas as pd
 
 # Set
 #path = 'C:/Users/Jae Ho/Desktop/hy-kist/Matrix_sound/test source_matixAADC'
@@ -28,15 +29,68 @@ screen = visual.Window([960, 900], screen=2, pos=[600, 0], fullscr=False,
                        monitor='testMonitor', color=[-1, -1, -1], blendMode='avg',
                        units='pix')
 
-#####################################  HTL  ######################################################
-text = visual.TextStim(screen, text="HTL \n\n puss space ", height=50, color=[1, 1, 1], wrapWidth=2000)
-text.draw()
-screen.flip()
-event.waitKeys(keyList=['space'], clearEvents=True)
+file = pd.read_excel(path + "/intro_HTLMCLSRT.xlsx")
 
+####### Intro exp #######
+for i in range(0,len(file)):
+    text = visual.TextStim(screen, text=file.Intro[i], height=30, color=[1, 1, 1], wrapWidth=2000)
+    n = file.Intro[i]
+
+    try:    # nan 즉 comment 끝이면 break
+        np.isnan(n)
+        break
+    except:
+        text.draw()
+        screen.flip()
+
+    key = event.waitKeys(keyList=["space", "escape"], clearEvents=True)
+    if key == ["escape"]:
+        core.quit()
+
+#####################################  HTL  ######################################################
+#text = visual.TextStim(screen, text="HTL \n\n puss space ", height=50, color=[1, 1, 1], wrapWidth=2000)
+#text.draw()
+#screen.flip()
+#event.waitKeys(keyList=['space'], clearEvents=True)
+port.write('H'.encode())
+
+####### Intro HTL #######
+for i in range(0,len(file)):
+    text = visual.TextStim(screen, text=file.HTL[i], height=30, color=[1, 1, 1], wrapWidth=2000)
+    n = file.HTL[i]
+
+    try:    # nan 즉 comment 끝이면 break
+        np.isnan(n)
+        break
+    except:
+        text.draw()
+        screen.flip()
+
+    key = event.waitKeys(keyList=["space", "escape"], clearEvents=True)
+    if key == ["escape"]:
+        core.quit()
+
+    while file.Num[i] == 4:    # 더미
+        port.write('0'.encode())
+
+        text = visual.TextStim(screen, text=" + ", height=150, color=[1, 1, 1], wrapWidth=2000)
+        text.draw()
+        screen.flip()
+        time.sleep(5)
+
+        text = visual.TextStim(screen, text=" 한번 더 들으시겠습니까? ", height=30, color=[1, 1, 1], wrapWidth=2000)
+        text.draw()
+        screen.flip()
+        key = event.waitKeys(keyList=["space", "1"], clearEvents=True)
+
+        if key == ["space"]:
+            break
+
+
+
+time.sleep(3)
 H_SPL = 0      # 첫 시작 SPL
 ## start HTL test
-port.write('H'.encode())
 while True:
 
     text = visual.TextStim(screen, text=" + ", height=150, color=[1, 1, 1], wrapWidth=2000)
@@ -51,7 +105,7 @@ while True:
     time.sleep(5)
 
     # Question
-    text = visual.TextStim(screen, text="들리나요? \n\n NO - 1 / OK -2", height=50, color=[1, 1, 1], wrapWidth=2000)
+    text = visual.TextStim(screen, text="들리나요? \n\n NO - 1 / OK - 2", height=50, color=[1, 1, 1], wrapWidth=2000)
     text.draw()
     screen.flip()
 
@@ -78,16 +132,16 @@ while True:
 print("HTL = {0}  dB SPL".format(int(H_SPL)))
 HTL = H_SPL
 scipy.io.savemat(path + '/hjy/SAVE/HTL_' + subject + '.mat', {'HTL_SPL': H_SPL})
-text = visual.TextStim(screen, text="HTL done", height=50, color=[1, 1, 1], wrapWidth=2000)
+text = visual.TextStim(screen, text="최소가청역치 검사가 끝났습니다.", height=50, color=[1, 1, 1], wrapWidth=2000)
 text.draw()
 screen.flip()
-time.sleep(3)
+#time.sleep(3)
 
 ######################################  MCL  ################################################################
 
-text = visual.TextStim(screen, text="MCL \n\n puss space ", height=50, color=[1, 1, 1], wrapWidth=2000)
-text.draw()
-screen.flip()
+#text = visual.TextStim(screen, text="MCL \n\n puss space ", height=50, color=[1, 1, 1], wrapWidth=2000)
+#text.draw()
+#screen.flip()
 event.waitKeys(keyList=['space'], clearEvents=True)
 print("========== MCL ===========")
 # HTL + 30 dB 부터
@@ -99,7 +153,26 @@ MCL_list = np.array([35,40,45,50,55,60,65,70,75,80])
 # for first track of MCL test
 
 idx = np.where(MCL_list == H_SPL)
-track = idx[0][0]+6
+track = idx[0][0]+8
+
+####### Intro MCL #######
+for i in range(0,len(file)):
+    text = visual.TextStim(screen, text=file.MCL[i], height=30, color=[1, 1, 1], wrapWidth=2000)
+    n = file.MCL[i]
+
+    try:  # nan 즉 comment 끝이면 break
+        np.isnan(n)
+        break
+    except:
+        text.draw()
+        screen.flip()
+
+    key = event.waitKeys(keyList=["space", "escape"], clearEvents=True)
+    if key == ["escape"]:
+        core.quit()
+
+
+time.sleep(3)
 
 ## start MCL test
 port.write('M'.encode())        # !! callibration 후에 첫 트랙번호가 두자리수 안넘아가는지 확인 !!
@@ -149,40 +222,90 @@ while True:
     print("-------------")
 
 MCL = H_SPL
+print("** MCL == {0} dB **".format(MCL))
 scipy.io.savemat(path + '/hjy/SAVE/MCL_' + subject + '.mat',
                  {'MCL_SPL': MCL})
-text = visual.TextStim(screen, text="MCL done", height=50, color=[1, 1, 1], wrapWidth=2000)
+text = visual.TextStim(screen, text="쾌적역치레벨 검사가 끝났습니다", height=50, color=[1, 1, 1], wrapWidth=2000)
 text.draw()
 screen.flip()
-time.sleep(3)
+event.waitKeys(keyList=['space'], clearEvents=True)
 ########################################  SRT_LEFT  ##############################################################
 
 # SRT test - LEFT
 # correct - 5 up / incorrect - 1 down
-text = visual.TextStim(screen, text="SRT_LEFT \n\n press space", height=50, color=[1, 1, 1], wrapWidth=2000)
+text = visual.TextStim(screen, text="쉬는 시간입니다. \n\n 안내에 따라주세요.", height=50, color=[1, 1, 1], wrapWidth=2000)
 text.draw()
 screen.flip()
-event.waitKeys(keyList=['space'], clearEvents=True)
 print("================================")
-print("========== SRT_RIGHT ===========")
+print("========== SRT_LEFT  ===========")
 print("=======  Change SD card! =======")
 print("================================")
+event.waitKeys(keyList=['space'], clearEvents=True)
+
+####### Intro SRT_L #######
+for i in range(0,len(file)):
+    text = visual.TextStim(screen, text=file.SRT_L[i], height=30, color=[1, 1, 1], wrapWidth=2000)
+    n = file.SRT_L[i]
+
+    try:    # nan 즉 comment 끝이면 break
+        np.isnan(n)
+        break
+    except:
+        text.draw()
+        screen.flip()
+
+    while file.Num[i] == len(file.SRT_L)-1:    # practice
+        time.sleep(3)
+        p = 1
+        # play
+        port.('P'.encode())
+        if p == 1:
+            text = visual.TextStim(screen, text="<<<", height=150, color=[1, 1, 1], wrapWidth=2000)
+
+        else:
+            text = visual.TextStim(screen, text=">>>", height=150, color=[1, 1, 1], wrapWidth=2000)
+
+        text.draw()
+        screen.flip()
+        time.sleep(4)
+        # respond
+        text = visual.TextStim(screen, text="따라해 \n\n 빨리", height=50, color=[1, 1, 1], wrapWidth=2000)
+        text.draw()
+        screen.flip()
+        # 내가 입력, 맞은 갯수를
+        resp = event.waitKeys(keyList=['0', '1', '2', '3', '4', '5'], clearEvents=True)    # correct
+        print('Correct = '+str(resp))
+        text = visual.TextStim(screen, text="next", height=50, color=[1, 1, 1], wrapWidth=2000)
+        text.draw()
+        screen.flip()
+        event.waitKeys(keyList=['space'], clearEvents=True)
+
+        p = p+1
+        if p == 3:
+            break
+
+    key = event.waitKeys(keyList=["space", "escape"], clearEvents=True)
+    if key == ["escape"]:
+        core.quit()
+
+time.sleep(3)
+
 #
 SRT_list = np.array([0,10,20,30,32,34,36,38,40,42,44,46,48,50])
 # Start SRT
 # LEFT attention
 ChargedB = 0
-H_SNR = -10     # first dB SNR
-track = 2
+H_SNR = 0     # first dB SNR
+track = 1
 case = 0
 SNRIdx = 0
-ChargeTrk = 2   # first track
+ChargeTrk = 0   # first track
 #port.write('S'.encode())
 SRTresp_L = np.zeros([14,])
 SRTfreq_L = np.zeros([14,])
 port.write('S'.encode())
 
-while track < 13*20:  # 30 track
+while track < 13*21:  # 20 track
 
     text = visual.TextStim(screen, text="<<<", height=150, color=[1, 1, 1], wrapWidth=2000)
     text.draw()
@@ -190,11 +313,12 @@ while track < 13*20:  # 30 track
 
     print(str(track)+'_SNR = '+str(H_SNR)+' dB')
     # Serial command
-    toArdCom = Alph_list[ChargeTrk-1]
+    toArdCom = Alph_list[ChargeTrk]
 
     CurrentTrack = track + SNRIdx
     print("Current Track = {0}".format(CurrentTrack))
     print("To Ard = {0}".format(toArdCom))
+    print(" + track = {0}".format(ChargeTrk))
     port.write(toArdCom.encode())   # arduino track에서 +할 숫자 보내
 
     time.sleep(5)
@@ -209,8 +333,8 @@ while track < 13*20:  # 30 track
 
     # dB 에 맞게 반응 넣기
     SNRIdx = int(np.where(SRT_list == -H_SNR)[0][0])
-    SRTfreq[SNRIdx] = SRTfreq[SNRIdx] + 1
-    SRTresp[SNRIdx] = SRTresp[SNRIdx] + int(resp[0])/5
+    SRTfreq_L[SNRIdx] = SRTfreq_L[SNRIdx] + 1
+    SRTresp_L[SNRIdx] = SRTresp_L[SNRIdx] + int(resp[0])/5
 
     if case == 0:
 
@@ -228,8 +352,8 @@ while track < 13*20:  # 30 track
             ChargedB = -2
 
     H_SNR = H_SNR + ChargedB
-    SNRIdx = int(np.where(SRT_list == -H_SNR)[0][0])-1  # exclude 0 dB
-    track = track + 13
+    SNRIdx = int(np.where(SRT_list == -H_SNR)[0][0])
+    track = track + 14
     ChargeTrk = (track + SNRIdx) - CurrentTrack     # 현재 arduino의 track 에서 + 시켜야할 num
 
     text = visual.TextStim(screen, text="next", height=50, color=[1, 1, 1], wrapWidth=2000)
@@ -262,14 +386,32 @@ print("=======  Change SD card! =======")
 print("================================")
 #
 
+####### Intro SRT_R #######
+for i in range(0,len(file)):
+    text = visual.TextStim(screen, text=file.SRT_R[i], height=30, color=[1, 1, 1], wrapWidth=2000)
+    n = file.SRT_R[i]
+
+    try:    # nan 즉 comment 끝이면 break
+        np.isnan(n)
+        break
+    except:
+        text.draw()
+        screen.flip()
+
+    key = event.waitKeys(keyList=["space", "escape"], clearEvents=True)
+    if key == ["escape"]:
+        core.quit()
+
+time.sleep(3)
+
 # Start SRT
 # LEFT attention
 ChargedB = 0
-H_SNR = -10     # first dB SNR
-track = 2
+H_SNR = 0     # first dB SNR
+track = 1
 case = 0
 SNRIdx = 0
-ChargeTrk = 2   # first track
+ChargeTrk = 0   # first track
 #port.write('S'.encode())
 SRTresp_R = np.zeros([14,])
 SRTfreq_R = np.zeros([14,])
@@ -283,7 +425,7 @@ while track < 13*20:  # 30 track
 
     print(str(track)+'_SNR = '+str(H_SNR)+' dB')
     # Serial command
-    toArdCom = Alph_list[ChargeTrk-1]
+    toArdCom = Alph_list[ChargeTrk]
 
     CurrentTrack = track + SNRIdx
     print("Current Track = {0}".format(CurrentTrack))
@@ -302,8 +444,8 @@ while track < 13*20:  # 30 track
 
     # dB 에 맞게 반응 넣기
     SNRIdx = int(np.where(SRT_list == -H_SNR)[0][0])
-    SRTfreq[SNRIdx] = SRTfreq[SNRIdx] + 1
-    SRTresp[SNRIdx] = SRTresp[SNRIdx] + int(resp[0])/5
+    SRTfreq_R[SNRIdx] = SRTfreq_R[SNRIdx] + 1
+    SRTresp_R[SNRIdx] = SRTresp_R[SNRIdx] + int(resp[0])/5
 
     if case == 0:
 
@@ -321,8 +463,8 @@ while track < 13*20:  # 30 track
             ChargedB = -2
 
     H_SNR = H_SNR + ChargedB
-    SNRIdx = int(np.where(SRT_list == -H_SNR)[0][0])-1  # exclude 0 dB
-    track = track + 13
+    SNRIdx = int(np.where(SRT_list == -H_SNR)[0][0])  # exclude 0 dB
+    track = track + 14
     ChargeTrk = (track + SNRIdx) - CurrentTrack     # 현재 arduino의 track 에서 + 시켜야할 num
 
     text = visual.TextStim(screen, text="next", height=50, color=[1, 1, 1], wrapWidth=2000)
--- Python/OnlySRT_withCEDA.py
+++ Python/OnlySRT_withCEDA.py
@@ -12,7 +12,7 @@ import time, serial, logging, scipy.io
 from sklearn.metrics import mean_squared_error
 from math import sqrt
 import serial, string, math
-
+import pandas as pd
 # Set
 #path = 'C:/Users/Jae Ho/Desktop/hy-kist/Matrix_sound/test source_matixAADC'
 path = 'C:/Users/LeeJiWon/Desktop/hykist/AAD/MatrixSentence'
@@ -29,8 +29,52 @@ screen = visual.Window([960, 900], screen=2, pos=[600, 0], fullscr=False,
                        winType='pyglet', allowGUI=False, allowStencil=False,
                        monitor='testMonitor', color=[-1, -1, -1], blendMode='avg',
                        units='pix')
-
+file = pd.read_excel(path + "/intro_HTLMCLSRT.xlsx")
 ########################################  SRT_LEFT  ##############################################################
+
+####### Intro SRT_L #######
+p = 1
+for i in range(0, len(file)):
+    text = visual.TextStim(screen, text=file.SRT_L[i], height=30, color=[1, 1, 1], wrapWidth=2000)
+    n = file.SRT_L[i]
+
+    while file.Num[i] == len(file.SRT_L):  # practice
+        time.sleep(3)
+        # play
+        port.write('P'.encode())
+        text = visual.TextStim(screen, text="<<<", height=150, color=[1, 1, 1], wrapWidth=2000)
+
+        text.draw()
+        screen.flip()
+        time.sleep(4)
+        # respond
+        text = visual.TextStim(screen, text="따라해 \n\n 빨리", height=50, color=[1, 1, 1], wrapWidth=2000)
+        text.draw()
+        screen.flip()
+        # 내가 입력, 맞은 갯수를
+        resp = event.waitKeys(keyList=['0', '1', '2', '3', '4', '5'], clearEvents=True)  # correct
+        print('Correct = ' + str(resp))
+        event.waitKeys(keyList=['space'], clearEvents=True)
+        text = visual.TextStim(screen, text="next", height=50, color=[1, 1, 1], wrapWidth=2000)
+        text.draw()
+        screen.flip()
+
+        p = p + 1
+        if p == 3:
+            break
+
+    try:  # nan 즉 comment 끝이면 break
+        np.isnan(n)
+        break
+    except:
+        text.draw()
+        screen.flip()
+
+    key = event.waitKeys(keyList=["space", "escape"], clearEvents=True)
+    if key == ["escape"]:
+        core.quit()
+
+time.sleep(3)
 
 # SRT test - LEFT
 # correct - 5 up / incorrect - 1 down
@@ -42,22 +86,23 @@ print("================================")
 print("========== SRT_RIGHT ===========")
 print("=======  Change SD card! =======")
 print("================================")
-#
+##
 SRT_list = np.array([0,10,20,30,32,34,36,38,40,42,44,46,48,50])
+Alph_list = list(string.ascii_lowercase)
 # Start SRT
 # LEFT attention
 ChargedB = 0
-H_SNR = -10     # first dB SNR
-track = 2
+H_SNR = 0     # first dB SNR
+track = 1
 case = 0
 SNRIdx = 0
-ChargeTrk = 2   # first track
+ChargeTrk = 0   # first track
 #port.write('S'.encode())
 SRTresp_L = np.zeros([14,])
 SRTfreq_L = np.zeros([14,])
 port.write('S'.encode())
 
-while track < 13*20:  # 30 track
+while track < 13*20:  # 20 track
 
     text = visual.TextStim(screen, text="<<<", height=150, color=[1, 1, 1], wrapWidth=2000)
     text.draw()
@@ -65,11 +110,12 @@ while track < 13*20:  # 30 track
 
     print(str(track)+'_SNR = '+str(H_SNR)+' dB')
     # Serial command
-    toArdCom = Alph_list[ChargeTrk-1]
+    toArdCom = Alph_list[ChargeTrk]
 
     CurrentTrack = track + SNRIdx
     print("Current Track = {0}".format(CurrentTrack))
     print("To Ard = {0}".format(toArdCom))
+    print(" + track = {0}".format(ChargeTrk))
     port.write(toArdCom.encode())   # arduino track에서 +할 숫자 보내
 
     time.sleep(5)
@@ -84,8 +130,8 @@ while track < 13*20:  # 30 track
 
     # dB 에 맞게 반응 넣기
     SNRIdx = int(np.where(SRT_list == -H_SNR)[0][0])
-    SRTfreq[SNRIdx] = SRTfreq[SNRIdx] + 1
-    SRTresp[SNRIdx] = SRTresp[SNRIdx] + int(resp[0])/5
+    SRTfreq_L[SNRIdx] = SRTfreq_L[SNRIdx] + 1
+    SRTresp_L[SNRIdx] = SRTresp_L[SNRIdx] + int(resp[0])/5
 
     if case == 0:
 
@@ -103,8 +149,8 @@ while track < 13*20:  # 30 track
             ChargedB = -2
 
     H_SNR = H_SNR + ChargedB
-    SNRIdx = int(np.where(SRT_list == -H_SNR)[0][0])-1  # exclude 0 dB
-    track = track + 13
+    SNRIdx = int(np.where(SRT_list == -H_SNR)[0][0])
+    track = track + 14
     ChargeTrk = (track + SNRIdx) - CurrentTrack     # 현재 arduino의 track 에서 + 시켜야할 num
 
     text = visual.TextStim(screen, text="next", height=50, color=[1, 1, 1], wrapWidth=2000)
@@ -125,32 +171,20 @@ time.sleep(3)
 
 ########################################  SRT_RIGHT  ##############################################################
 
-# SRT test - RIGHT
-# correct - 5 up / incorrect - 1 down
-text = visual.TextStim(screen, text="SRT_RIGHT \n\n press space", height=50, color=[1, 1, 1], wrapWidth=2000)
-text.draw()
-screen.flip()
-event.waitKeys(keyList=['space'], clearEvents=True)
-print("================================")
-print("========== SRT_RIGHT ===========")
-print("=======  Change SD card! =======")
-print("================================")
-#
-
 # Start SRT
 # LEFT attention
 ChargedB = 0
-H_SNR = -10     # first dB SNR
-track = 2
+H_SNR = 0     # first dB SNR
+track = 1
 case = 0
 SNRIdx = 0
-ChargeTrk = 2   # first track
-#port.write('S'.encode())
+ChargeTrk = 0   # first track
+
 SRTresp_R = np.zeros([14,])
 SRTfreq_R = np.zeros([14,])
 port.write('S'.encode())
 
-while track < 13*20:  # 30 track
+while track < 13*20:  # 20 track
 
     text = visual.TextStim(screen, text=">>>", height=150, color=[1, 1, 1], wrapWidth=2000)
     text.draw()
@@ -158,11 +192,12 @@ while track < 13*20:  # 30 track
 
     print(str(track)+'_SNR = '+str(H_SNR)+' dB')
     # Serial command
-    toArdCom = Alph_list[ChargeTrk-1]
+    toArdCom = Alph_list[ChargeTrk]
 
     CurrentTrack = track + SNRIdx
     print("Current Track = {0}".format(CurrentTrack))
     print("To Ard = {0}".format(toArdCom))
+    print(" + track = {0}".format(ChargeTrk))
     port.write(toArdCom.encode())   # arduino track에서 +할 숫자 보내
 
     time.sleep(5)
@@ -177,8 +212,8 @@ while track < 13*20:  # 30 track
 
     # dB 에 맞게 반응 넣기
     SNRIdx = int(np.where(SRT_list == -H_SNR)[0][0])
-    SRTfreq[SNRIdx] = SRTfreq[SNRIdx] + 1
-    SRTresp[SNRIdx] = SRTresp[SNRIdx] + int(resp[0])/5
+    SRTfreq_R[SNRIdx] = SRTfreq_R[SNRIdx] + 1
+    SRTresp_R[SNRIdx] = SRTresp_R[SNRIdx] + int(resp[0])/5
 
     if case == 0:
 
@@ -196,8 +231,8 @@ while track < 13*20:  # 30 track
             ChargedB = -2
 
     H_SNR = H_SNR + ChargedB
-    SNRIdx = int(np.where(SRT_list == -H_SNR)[0][0])-1  # exclude 0 dB
-    track = track + 13
+    SNRIdx = int(np.where(SRT_list == -H_SNR)[0][0])
+    track = track + 14
     ChargeTrk = (track + SNRIdx) - CurrentTrack     # 현재 arduino의 track 에서 + 시켜야할 num
 
     text = visual.TextStim(screen, text="next", height=50, color=[1, 1, 1], wrapWidth=2000)
@@ -206,6 +241,7 @@ while track < 13*20:  # 30 track
     event.waitKeys(keyList=['space'], clearEvents=True)
 
     print("----------------")
+
 
 #print('SRT = ' + str(SRT) + ' dB SNR')
 scipy.io.savemat(path + '/hjy/SAVE/SRTfreq_R' + subject + '.mat', {'SRTfreq_R': SRTfreq_R})
